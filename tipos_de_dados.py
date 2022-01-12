3e14ab5213e835dd81cc9ba1e104d4c0a07cc822
Gugonunes <gugonunes@hotmail.com>
2022-01-12 18:27:35 -0300

    Create tipos_de_dados.py
    Tipos de dados usados em Python.
    O comando "type" mostra no terminal qual é o tipo de dado em questão.

---
+++ tipos_de_dados.py
@@ -0,0 +1,12 @@
+"""
+Tipos de dados
+str - string  -  textos 'assim' ou "assim"
+int - inteiro  -  123456 0 -1323
+float - numero real/flutuante  -  10.2  -20.2  0.0
+bool - booleano/lógico  -  True ou False  10 == 10
+"""
+
+print("Gustavo", type("Gustavo"))
+print(10, type(10))
+print(10.2, type(10.2))
+print('l' == 'l', type('l' == 'l'))
