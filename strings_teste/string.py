46801b4bceb95df533f9c944798fcf319ea1b5c6
Gugonunes <gugonunes@hotmail.com>
2022-01-12 18:51:50 -0300

    Create string.py

---
+++ strings_teste/string.py
@@ -0,0 +1,11 @@
+"""
+str - string
+tudo que da dentro de aspas
+"""
+
+print('Essa é uma "string" (str).')  #para por aspas no print, basta por uma diferente da usada pra começar a string
+print("Essa é uma 'string' (str).")
+print("Esse é meu \"texto\" (str).")  #'\' é um caractere de escape, o que vem dps da \ nao é interpretado como 'função'
+print(r"esse é meu texto com \n")  #o r antes das aspas ignora o caractere de escape
+
+#o melhor é usar a troca de aspas
