d85a231c5454160f9bdae417f0ef321a6bcb2a69
Gugonunes <gugonunes@hotmail.com>
2022-10-29 21:10:00 -0300

    ajustes no exercicio da aula 58

--- Nível Intermediário/Exercicios Propostos/Ex2_aula58.py
+++ Nível Intermediário/Exercicios Propostos/Ex2_aula58.py
@@ -14,13 +14,14 @@ print('----------------------------------------')
 2- Crie uma função1 que recebe uma função2 como parâmetro e retorne o valor da função2 executada. Faça a função1 executar duas funções
 que recebam um número diferente de argumentos
 """
-def func2_1(func_aux2, func_aux2_2):
-    return (func_aux2('gustavo', 'ola'), func_aux2_2('22'))
+def func2_1(func_aux2, *args, **kwargs):
+    return (func_aux2(*args, **kwargs))
 
 def func2_2(nome, saudacao):
-    print(f'{saudacao} {nome}')
+    return (f'{saudacao} {nome}')
 
 def func2_3(idade):
-    print(f'Voce tem {idade} anos!')
+    return (f'Voce tem {idade} anos!')
 
-func2_1(func2_2, func2_3)
+print(func2_1(func2_2, 'Gustavo', 'Olá'))
+print(func2_1(func2_3, idade = '22'))
