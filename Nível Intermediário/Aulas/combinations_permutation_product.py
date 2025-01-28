f267618225679b9485a14e134dbebe7e6d254790
gugonunes <gugonunes@hotmail.com>
2025-01-28 20:01:31 -0300

    feat(intermediate): add combinations, permutation and product

---
+++ Nível Intermediário/Aulas/combinations_permutation_product.py
@@ -0,0 +1,23 @@
+# combination: ordem não importa
+# permutation: order importa
+# product: ordem importa e repete valores unicos
+
+from itertools import combinations, permutations, product
+
+def print_iter(iterator):
+    print(*list(iterator), sep='\n')
+    print()
+
+pessoas = [
+    'João', 'Joana', 'Luiz', 'Leticia',
+]
+camisetas = [
+    ['preta', 'branca'],
+    ['p', 'm', 'g'],
+    ['masculino', 'feminino', 'unisex'],
+    ['algodão', 'poliéster']
+]
+
+print_iter(combinations(pessoas, 2))
+print_iter(permutations(pessoas, 2))
+print_iter(product(*camisetas))
